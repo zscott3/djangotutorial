f7a6b5ee83fd3caf9229a9d176a7b05c9f52b494
zscott <zscott3@ivytech.edu>
2024-02-10 20:27:15 -0500

    My Django first commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
